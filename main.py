a5be148fa9fec80bf8259cb7dbe4cbfa2f086fef
Dongjin-1203 <hambur1203@gmail.com>
2025-05-27 13:48:24 +0900

    결측치 제거기능 추가

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from module.data_EDA import data_EDA
 if __name__ == "__main__":
     print("모듈을 성공적으로 불러왔습니다.")
     path = 'data/netflix_titles.csv'
-    load_data(path)
+    data_EDA(path)
     
--- module/data_EDA.py
+++ module/data_EDA.py
@@ -1,17 +1,31 @@
 import pandas as pd
+import numpy as np
 
 def data_EDA(path):
     # 데이터 불러오기
     data = pd.read_csv(path)
 
-    #데이터 확인
+    # 데이터 확인
     print("데이터를 성공적으로 불러왔습니다.")
     print(data.head())
     print(data.info())
 
-    #결측치 비율 확인
+    # 결측치 비율 확인
     for i in data.columns:
         nullValueRate = data[i].isna().sum() / len(data) * 100
         if nullValueRate > 0:
-            print("{} null rate: {}%".format(i, round(nullValueRate, 2)))
+            print("{} null rate: {}%".format(i, round(nullValueRate, 2)))   # 결측치 비율 출력
+        
+    #'country', 'director', 'cast'가 결측치 비율이 높은것으로 확인
+
+    # 결측치 처리
+
+    # 치환
+    data['country'] = data['country'].fillna('No data')
+    data['director'] = data['director'].replace(np.nan, 'No data')
+    data['cast'] = data['cast'].replace(np.nan, 'No data')
+
+    # 제거
+    data.dropna(axis=0, inplace=True)
+    print(data.isna().sum())
     return print("finish EDA")
